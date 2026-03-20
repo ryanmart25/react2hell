5f192b397a93464976daa24e2daf0b42bce05fb6
ChurchDuck1 <bchurch2@csus.edu>
2026-03-20 16:10:32 -0700

    working on MySQL

--- Spoonomics/app/build.gradle.kts
+++ Spoonomics/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room)
 }
 
 android {
@@ -55,4 +57,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
